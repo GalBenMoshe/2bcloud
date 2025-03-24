304dcc9a107d408fc63d382164c9f3b6fc655cb5
gal <gal@gmail.com>
2025-03-24 18:35:28 +0200

    check

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -14,7 +14,7 @@
 
 terraform {
   backend "azurerm" {
-    resource_group_name  = "MC_Gal-Candidate_aks-cluster_westeurope"
+    resource_group_name  = "Gal-Candidate"
     storage_account_name = "tfbackendstoragegal"
     container_name       = "tfstate"
     key                  = "terraform.tfstate"
--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,7 +1,7 @@
 variable "resource_group_name" {
   description = "Name of the Azure resource group"
   type        = string
-  default     = "MC_Gal-Candidate_aks-cluster_westeurope"
+  default     = "Gal-Candidate"
 }
 
 variable "location" {
