1a0b0c7fc1e293d07bc4992478463d9a4c55d6d9
Gal Ben Moshe <111042068+GalBenMoshe@users.noreply.github.com>
2025-03-26 15:39:09 +0200

    Clean backend.tf

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,17 +1,3 @@
-# resource "azurerm_storage_account" "tf_backend" {
-#   name                     = "tfbackendstoragegal"
-#   resource_group_name      = var.resource_group_name
-#   location                 = var.location
-#   account_tier             = "Standard"
-#   account_replication_type = "LRS"
-# }
-
-# resource "azurerm_storage_container" "tf_state" {
-#   name                 = "tfstate"
-#   storage_account_name   = azurerm_storage_account.tf_backend.name
-#   container_access_type = "private"
-# }
-
 terraform {
   backend "azurerm" {
     resource_group_name  = "MC_Gal-Candidate_aks-cluster_westeurope"
